ce105c158db925b3d8ef82c78ef88428a2b4ba14
Josef-Ondrej <josef.ondrej@ibm.com>
2019-05-23 15:03:31 +0200

    Update formatting and readme

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-Kubernetes .yaml deployment formatter
+Kubernetes .yaml Deployment Formatter
 ======================================
+
+Small utility that makes the kubernetes deployment yaml files more human readable. 
 
 Use like:
 
 `file.yaml | python yaml_format.py`
 
+Personally I have an alias in `.bashrc`
+
+`alias yform='python /home/josef/dev/yaml_formatter/yaml_format.py`
+
+and then I can do 
+
+`file.yaml | yform` 
--- yaml_format.py
+++ yaml_format.py
@@ -1,41 +1,56 @@
 import yaml
 from colorama import Fore, Style
-from typing import Dict
+from typing import Dict, List
 import sys
 
 VALUE_MAX_LEN = 100
 
-def format_container(container: Dict, prefix: str = "", is_init: bool = False) -> str:
+
+def format_env_vars(env_vars: List, prefix: str = "") -> str:
     output = ""
-    container_flag = f"{Fore.YELLOW}INIT CONTAINER{Style.RESET_ALL}" if is_init else f"{Fore.YELLOW}CONTAINER{Style.RESET_ALL}"
-    container_name = container["name"]
-    image = container["image"]
-    command = container.get("command") or "No command"
-    output += prefix + f"{container_flag} [ {container_name} ]\n"
-    output += prefix + f"{Fore.CYAN}Image:{Style.RESET_ALL} {image}\n"
-    output += prefix + f"{Fore.CYAN}Command: {Style.RESET_ALL} {command}\n"
-
-    output += prefix + f"{Fore.CYAN}Volume mounts: {Style.RESET_ALL}\n"
-    for volumeMount in container["volumeMounts"]:
-        path = volumeMount["mountPath"]
-        name = volumeMount["name"]
-        output += prefix + f"  {path} < {Fore.MAGENTA}{name}{Style.RESET_ALL}\n"
-
-    output += prefix + f"{Fore.CYAN}Envirnomental variables: {Style.RESET_ALL}\n"
-    for env in container["env"]:
+    for env in env_vars:
         env_name = env["name"]
         env_value = env.get("value") or "valueFrom ..."
         if len(env_value) > VALUE_MAX_LEN:
             env_value = env_value[:VALUE_MAX_LEN] + "..."
-        output += prefix + f"  {Fore.MAGENTA}{env_name}{Style.RESET_ALL} = {env_value}\n"
+        output += prefix + f"{Fore.LIGHTMAGENTA_EX}{env_name}{Style.RESET_ALL} = {env_value}\n"
+
+    return output
+
+
+def format_volume_mounts(volume_mounts: List, prefix: str = "") -> str:
+    output = ""
+    for volumeMount in volume_mounts:
+        path = volumeMount["mountPath"]
+        name = volumeMount["name"]
+        output += prefix + f"{path} < {Fore.LIGHTMAGENTA_EX}{name}{Style.RESET_ALL}\n"
+
+    return output
+
+
+def format_container(container: Dict, prefix: str = "") -> str:
+    image = container["image"]
+    command = container.get("command") or "No command"
+
+    output = ""
+    output += prefix + f"{Fore.LIGHTCYAN_EX}Image:{Style.RESET_ALL} {image}\n"
+    output += prefix + f"{Fore.LIGHTCYAN_EX}Command: {Style.RESET_ALL} {command}\n"
+
+    output += prefix + f"{Fore.LIGHTCYAN_EX}Volume mounts: {Style.RESET_ALL}\n"
+    output += format_volume_mounts(container["volumeMounts"], 2 * prefix)
+
+    output += prefix + f"{Fore.LIGHTCYAN_EX}Envirnomental variables: {Style.RESET_ALL}\n"
+    output += format_env_vars(container["env"], 2 * prefix)
 
     output += "\n"
     return output
 
+
 def format_volume(volume: Dict, prefix: str = "") -> str:
-    output = ""
     volume_name = volume["name"]
-    output += prefix + f"{Fore.GREEN}{volume_name}{Style.RESET_ALL}"
+
+    output = ""
+    output += prefix + f"{Fore.LIGHTMAGENTA_EX}{volume_name}{Style.RESET_ALL}"
 
     if volume.get("secret") is not None:
         volume_type = "secret"
@@ -57,22 +72,24 @@ def format_deploy(deploy: Dict, prefix: str = "") -> str:
 
     output = ""
     for container in containers:
+        container_name = container["name"]
+        output += prefix + f"{Fore.LIGHTYELLOW_EX}CONTAINER{Style.RESET_ALL} [{container_name}]\n"
         output += format_container(container, prefix)
 
     for container in init_containers:
-        output += format_container(container, prefix, True)
+        output += prefix + f"{Fore.LIGHTYELLOW_EX}INIT CONTAINER{Style.RESET_ALL} [{container_name}]\n"
+        output += format_container(container, prefix)
 
-    output += prefix + f"{Fore.YELLOW}VOLUMES{Style.RESET_ALL}\n"
+    output += prefix + f"{Fore.LIGHTYELLOW_EX}VOLUMES{Style.RESET_ALL}\n"
     for volume in volumes:
         output += format_volume(volume, prefix)
 
     return output
 
+
 if __name__ == "__main__":
     data = "".join(sys.stdin.readlines())
     deploy = yaml.safe_load(data)
-    formatted_deploy = format_deploy(deploy, "  ")
+    formatted_deploy = format_deploy(deploy, "    ")
 
     print(formatted_deploy)
-
-
